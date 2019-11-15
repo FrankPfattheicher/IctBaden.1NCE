639c6b4c953884db49d87d10c957104bbebf5dc0
Frank Pfattheicher <fpf@ict-baden.de>
2019-11-15 18:47:27 +0100

    Initial project setup.

---
+++ src/IctBaden.1NCE/IctBaden.1NCE.Dashboard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IctBaden._1NCE.Dashboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/IctBaden.1NCE/IctBaden.1NCE/Urls.cs
@@ -0,0 +1,12 @@
+﻿
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+namespace IctBaden._1NCE
+{
+    public static class Urls
+    {
+        public const string Authentication = "https://api.1nce.com/management-api/oauth/token";
+        public const string Api = "https://api.1nce.com";
+    }
+}
