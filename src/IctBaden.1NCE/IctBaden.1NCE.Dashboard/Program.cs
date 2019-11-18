f4626034375724b115354b894a4a3a6178cb6c48
Frank Pfattheicher <fpf@ict-baden.de>
2019-11-18 23:50:12 +0100

    SimsV1 api first method.

--- src/IctBaden.1NCE/IctBaden.1NCE.Dashboard/Program.cs
+++ src/IctBaden.1NCE/IctBaden.1NCE.Dashboard/Program.cs
@@ -1,18 +1,26 @@
 ﻿using System;
+using System.Linq;
 using IctBaden.Api1NCE;
 
 namespace IctBaden._1NCE.Dashboard
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("1NCE API Dashboard");
 
             var usr = Environment.GetEnvironmentVariable("API_1NCE_USER");
             var pwd = Environment.GetEnvironmentVariable("API_1NCE_PASSWORD");
             var token = ApiAuthentication.GetToken(DefaultUrls.Authentication, usr, pwd);
+        
+            var sims = new SimsV1(DefaultUrls.Api, token);
+            var list = sims.GetSimCards();
             
+            foreach (var simCard in list.OrderBy(sc => sc.IMSI))
+            {
+                Console.WriteLine($"{simCard.IMSI} : {simCard.Label}");
+            }
         }
     }
 }
---
+++ src/IctBaden.1NCE/IctBaden.1NCE/SimsV1.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using IctBaden.Api1NCE.Models;
+using Newtonsoft.Json;
+
+namespace IctBaden.Api1NCE
+{
+    public class SimsV1 : IDisposable
+    {
+        private readonly string _apiUrl;
+        private readonly WebClient _client;
+
+        public SimsV1(string apiUrl, string accessToken)
+        {
+            _apiUrl = apiUrl;
+            _client = new WebClient();
+            
+            _client.Headers.Add(HttpRequestHeader.Accept, "application/json");
+            _client.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + accessToken);
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();    
+        }
+        
+        public List<SimCard> GetSimCards()
+        {
+            try
+            {
+                var json = _client.DownloadString(_apiUrl + "/management-api/v1/sims");
+
+                var simCards = JsonConvert.DeserializeObject<List<SimCard>>(json);
+
+                return simCards;
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError("GetSimCards: " + ex.Message);
+                return null;
+            }
+        }
+        
+        
+    }
+}
